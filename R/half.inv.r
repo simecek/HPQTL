6339a5aba19034035bda949406f31c660777f56b
rakosnicek <lamparna@gmail.com>
2014-05-12 22:00:52 -0400

    half.inv and variance.decomposition functions now separated from scan1

---
+++ R/half.inv.r
@@ -0,0 +1,29 @@
+#' W^(-1/2) matrix operation
+#' 
+#' @author copied from QTLRel
+#'  
+#' @param W matrix
+#'
+#' @references Abney M, Ober C, McPeek MS (2002). "Quantitative trait homozygosity
+#' and association mapping and empirical genome-wide significance in
+#' large complex pedigrees: Fasting serum insulin levels in the
+#' Hutterites." American Journal of Human Genetics 70: 920-934.
+#'
+#' @return matrix \code{A} such that \code{t(A)%*%A = W^{-1}}
+#' 
+#' @keywords manip
+#'
+#' @examples
+#' cross <- sim.cross.geno(250, nmar=10)
+#' cross$pheno <- sim.cross.pheno(0.5, cross)
+#' heritability(cross)
+
+half.inv <- function(W, symmetric=TRUE,inverse=TRUE){
+  eW <- eigen(W, symmetric=symmetric)
+  d <- eW$values
+  if (min(d) <0  && abs(min(d))>sqrt(.Machine$double.eps))
+    stop("'W' is not positive definite")
+  else d[d<=0]<- ifelse(inverse, Inf, 0)
+  A <- diag(d^ifelse(inverse, -0.5, 0.5)) %*% t(eW$vector)
+  A # t(A)%*%A = W^{-1}
+}
---
+++ R/variance.decomposition.r
@@ -0,0 +1,40 @@
+#' Decompose variance into genetic part and a random noise
+#' 
+#' @param y a vector with phenotype
+#' @param covar a matrix with covariates
+#' @param G genetic similarity matrix
+#' @param package / method to do variant component calculations
+#' 
+#' @details Currently, \code{regress} function is much faster than \code{QTLRel::estVC}
+#' but it prints 
+#' 
+#' 
+#' @return 4-elements list
+#' 
+#' @keywords manip
+#'
+#' @examples
+#' cross <- sim.cross.geno(250, nmar=10)
+#' cross$pheno <- sim.cross.pheno(0.5, cross)
+#' heritability(cross)
+
+variance.decomposition <- function(y, covar, G, package = c("regress", "QTLRel")) {
+  package = match.arg(package)
+  
+  if (package=="regress") {
+    fit <- regress(y~.,~G, data=as.data.frame(cbind(y=y, covar))) 
+    V <- fit$sigma["G"] * G + fit$sigma["In"] * diag(nrow(G))
+    A <- half.inv(V)
+    return(list(V=V, A=A, var.g = fit$sigma["G"], var.e = fit$sigma["In"]))
+  } else {
+    EE <- diag(nrow(G))
+    if (is.null(covar)) {
+      vc <- estVC(y=pheno, v=list(AA=G, DD=NULL, HH=NULL, AD=NULL, MH=NULL, EE=EE))
+    } else {
+      vc <- estVC(y=pheno, covar, v=list(AA=G, DD=NULL, HH=NULL, AD=NULL, MH=NULL, EE=EE))
+    }  
+    V <- vc$par["AA"]*G + vc$par["EE"]*EE
+    A <- half.inv(V)
+    return(list(V=V, A=A, var.g = vc$par["AA"], var.e = vc$par["EE"]))
+  }  
+}
