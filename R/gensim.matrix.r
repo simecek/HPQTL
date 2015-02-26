4d0e380dc70542d78901773780df5be8e1dab8e9
simecek <lamparna@gmail.com>
2015-02-26 12:14:43 -0500

    simplified to one possible GSM calculation

--- R/gensim.matrix.r
+++ R/gensim.matrix.r
@@ -2,7 +2,6 @@
 #' 
 #'   
 #' @param geno genotype probabilities ("\code{genotype.probs}" or "\code{cross}" object)
-#' @param method how to calculate similarity matrix
 #' @param procedure procedure of scan1 the G is calculated for
 #' @param subjects subseting of subjects
 #' @param markers subseting of markers
@@ -22,87 +21,43 @@
 #' G <- gensim.matrix(geno)
 #' Glist <- gensim.matrix(geno, procedure="LOCO")
 
-gensim.matrix <- function(geno, method=c('default', 'allele-2f-additive', 'allele-multif-additive', 'allele-2f-weighted', 'allele-multif-cosine'),
-                          procedure = c("LMM", "LOCO", "LM","POMOLOCO"), subjects=seq(geno$subjects), markers = seq(NROW(geno$markers)), 
-                          gensim.normalization="sample-variance", ...) {
+gensim.matrix <- function(geno, procedure = c("LMM", "LOCO", "POMOLOCO", "LM"),  
+                          gensim.normalization="sample-variance", verbose = FALSE) {
   
   # match arguments
-  method = match.arg(method) 
   procedure = match.arg(procedure)
   
   # if not genotype.probs then extract genotype probs
   if (!("genotype.probs" %in% class(geno))) geno <- extract.geno(geno)
   check.genotype.probs(geno)
-
-  # for default 'method' guess 
-  if (method=="default")
-    if (length(geno$calls)<=3) 
-      method <- 'allele-2f-weighted'
-    else 
-      method <- 'allele-multif-cosine'
-    
+  
   # for linear model no genetic similarity matrix is needed
   if (procedure == "LM") return(NULL)
+
+  # calculate GSM per chromosome
+  GSM.per.chr <- list()
+  for (c in geno$chromosomes$chr) {
+    if (verbose) message(paste("Processing GSM of "))
+    tmp <- geno$probs[,,geno$markers$chr==c]
+    dim(tmp) <- c(dim(tmp)[1], dim(tmp)[2] * dim(tmp)[3])
+    GSM.per.chr[[c]] <- tcrossprod(tmp)
+  }
   
-  # for LOCO call it recursively for each chromosome
+  if (procedure == "LMM") {
+    G = Reduce("+", GSM.per.chr)
+    return(normalize.matrix(G, method = gensim.normalization))
+  }
+  
   if (procedure == "LOCO") {
     Glist <- list()
     for (c in geno$chromosomes$chr)
-      Glist[[c]] <- gensim.matrix(geno, method=method, procedure="LMM", subjects=subjects, 
-                                  markers = intersect(markers,which(geno$markers$chr!=c)), 
-                                  gensim.normalization=gensim.normalization)
-    return(Glist)
+      Glist[[c]] <- Reduce("+", GSM.per.chr[geno$chromosomes$chr!=c])
+    return(normalize.matrix(Glist, method = gensim.normalization))
   }
   
-  # for POMOLOCO call it recursively for each chromosome
   if (procedure == "POMOLOCO") {
-    Glist <- list()
-    for (c in geno$chromosomes$chr)
-      Glist[[c]] <- gensim.matrix(geno, method=method, procedure="LMM", subjects=subjects, 
-                                  markers = intersect(markers,which(geno$markers$chr==c)), 
-                                  gensim.normalization=gensim.normalization)
-    return(Glist)
-  }
-  
-  if (method == 'allele-2f-additive') {
-    if (length(geno$calls)<2 | length(geno$calls)>3) stop("Method 'allele-2f-additive' expects 2 founders.")
-    score.matrix <- switch(length(geno$calls),
-                           NULL,
-                           matrix(c(1,1/2,1/2,1),2,2),                     
-                           matrix(c(1,1/2,0,1/2,1,1/2,0,1/2,1),3,3)) 
-    K <- matrix(0, length(subjects), length(subjects))
+    return(GSM.per.chr)
+  }  
     
-    for (j in markers) # for each snp get kinship
-      K <- K + geno$probs[subjects,,j] %*% score.matrix %*% t(geno$probs[subjects,,j])    
-    
-  }
-  
-  if (method == 'allele-2f-weighted') {
-    if (length(geno$calls)<2 | length(geno$calls)>3) stop("Method 'allele-2f-additive' expects 2 founders.")
-    
-    f <- function(A) colSums(t(A) * (1:ncol(A)))
-    Zt <- scale(t(apply(geno$probs[subjects,,markers],3, f)))
-    K <- t(Zt) %*% Zt   
-  }
-  
-  if (method == 'allele-multif-additive') {
-    score.matrix <- diag(length(geno$calls))
-    K <- matrix(0, length(subjects), length(subjects))
-    for (j in markers) # for each snp get kinship
-      K <- K + geno$probs[subjects,,j] %*% score.matrix %*% t(geno$probs[subjects,,j])
-    
-  }
-  
-  if (method == 'allele-multif-cosine') {
-    mprobs = geno$probs[subjects,,markers]
-    dp = dim(mprobs)
-    dim(mprobs) = c(dp[1], dp[2]*dp[3])
-    K <- mprobs %*% t(mprobs)
-  }
-  
-  # normalize the matrix
-  K <- normalize.matrix(K, method=gensim.normalization)
-  
-  return(K)
-  
+  stop("Something wrong happened.")
 }
